53b0102c3d8936c6545eb509f95b3b32f82bc4bf
mangalam.rstrnt <mangalam.rstrnt@gmail.com>
2025-12-16 14:42:54 +0000

    chore: touch server/index.js to force rebuild

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ app.use(cors());
 // ROUTES
 app.use("/api/auth", require("./routes/authRoutes"));
 
+// Health check endpoint
 app.get('/', (req, res) => {
   res.status(200).send('Mangalam WiFi Zone Backend is running');
 });
